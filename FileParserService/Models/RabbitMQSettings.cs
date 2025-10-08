5415efdfaa113f4216dc0a8d1c44f7715415bc95
Kerven Jumayev <kerwen.jumazew@gmail.com>
2025-10-08 16:52:29 +0300

    update RabbitMQ settings

--- FileParserService/Models/RabbitMQSettings.cs
+++ FileParserService/Models/RabbitMQSettings.cs
@@ -4,6 +4,9 @@
     {
         public string HostName { get; set; }
         public string Exchange { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
         public string RoutingKey { get; set; }
     }
 }
